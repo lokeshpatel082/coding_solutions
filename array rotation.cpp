257322fe605d1244c13c5e14fcff96d3f85c378a
Ankit Bhankharia <71999020+cupcake08@users.noreply.github.com>
2020-10-31 01:25:42 +0530

    Improved rotateArray.cpp
    It will rotate array from user defined index.
    Hope you find it  helpful

--- array rotation.cpp
+++ array rotation.cpp
@@ -1,32 +1,43 @@
 #include<iostream>
 using namespace std;
+void swap(int &x,int &y)  //swap two numbers
+{
+    int temp=x;
+    x=y;
+    y=temp;
+}
+void reverse(int *arr,int si,int ei)  //reverses array from si(start Index) to ei(end Index)
+{
+    while(si<ei)
+    {
+        swap(arr[si],arr[ei]);
+        si++;
+        ei--;
+    }
+}
+void rotateArray(int *arr,int size,int k)   
+{
+    int si=0;
+    int ei=size;
+    reverse(arr,si,ei);   //reverse complete array
+    reverse(arr,0,size-k);   //reverse array from index zero to size-k
+    reverse(arr,size-k+1,size);   //reverse array from size-K+1 to size
+}
 int main()
 {
-    int n,r=0,temp=0,k=0;
-    cin>>n>>r;
-    int a[n];
+    int n;
+    cin>>n;
+    int *arr=new int[n];
     for(int i=0;i<n;i++)
     {
-        cin>>a[i];
+        cin>>arr[i];
     }
-    for(int l=0;l<r;l++)
+    int k;  //index number from where array to be rotate
+    cin>>k;
+    rotateArray(arr,n-1,k);
+    for(int i=0;i<n;i++)
     {
-        temp=a[0];
-       for(int i=0;i<n;i++)
-       {
-
-          k=i+1;
-          if(i+1==n)
-          a[i]=temp;
-          else
-          a[i]=a[k];
-       }
-
+        cout<<arr[i]<<" ";
     }
-    for(int l=0;l<n;l++)
-    {cout<<a[l]<<" ";
-    }
-
-    return 0;
-
+    delete[] arr;
 }
